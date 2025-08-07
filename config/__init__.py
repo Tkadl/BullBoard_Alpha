ee60c0a4e1a67b7dc2d7023246734551b421c67a
Tkadl <139879287+Tkadl@users.noreply.github.com>
2025-08-07 10:43:25 +0200

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+"""
+Configuration package for BullBoard application
+Contains all constants, styling, and configuration settings
+"""
+
+from .constants import *
+from .settings import *
+
+__version__ = "1.0.0"
